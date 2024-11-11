a8479f4f13a08e1e1ffbfae772ae595e7f73dc1a
koylear <koylear@wgu.edu>
2024-11-10 16:16:11 -0800

    update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def go(config: DictConfig):
                         "trainval_artifact": "trainval_data.csv:latest",
                         "output_artifact": "random_forest_export",
                         "test_size": config['modeling']['test_size'],
-                        "val_size": config['modeling']['val_size']
+                        "val_size": config['modeling']['val_size'],
                         "random_seed": config['modeling']['random_seed'],
                         "stratify_by": config['modeling']['stratify_by'],
                         "max_tfidf_features": config['modeling']['max_tfidf_features'],
